b9faf07c96243e57af8cbf4ed69fbec369bad165
Eduardo Moreira <contato@eduardoem.com.br>
2022-03-19 15:10:15 -0300

    Fix cors heroku issue

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,5 @@
+const { WebpackPluginServe: Serve } = require('webpack-plugin-serve');
+
 module.exports = {
   webpack: (config, env) => {
     config.optimization.runtimeChunk = false
@@ -11,6 +13,17 @@ module.exports = {
 
     config.plugins[5].options.filename = 'static/css/[name].css'
     config.plugins[5].options.moduleFilename = () => 'static/css/main.css'
+
+    config.plugins.push(new Serve({
+      middleware: (app, builtins) =>
+        app.use(async (ctx, next) => {
+          await next();
+
+          ctx.set('Access-Control-Allow-Origin', '*');
+          ctx.set('Access-Control-Allow-Headers', '*');
+          ctx.set('Access-Control-Allow-Methods', '*');
+        }),
+    }))
 
     return config
   },
